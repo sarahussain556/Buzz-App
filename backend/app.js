9ccd16fa40a2db3c8090fcfb1bffadeedde7e05f
Sara Hussain <85155050+sarahussain556@users.noreply.github.com>
2022-04-19 00:26:50 +0530

    Connecting to mongoDB
    adding .env file and pasting URL from our mongoDB cluster.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
-const express =require("express");
+const express =require("express");  //node js framework, our server will run on this express
 const app=express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config(); //to use dotenv
+
+mongoose.connect(
+  process.env.DATABASE,
+
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
 app.listen(8800,()=>{
   console.log("backend server is running");
