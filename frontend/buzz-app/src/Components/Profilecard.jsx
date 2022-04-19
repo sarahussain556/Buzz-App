856693404e5f838cfd78ac711bf281611f0d8bca
Prakashttn <prakash.chand@tothenew.com>
2022-04-19 15:31:41 +0530

    Aligned leftsidebar and started profile

---
+++ frontend/buzz-app/src/Components/Profilecard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Profilecard = () => {
+  return (
+      <>
+      
+      <div>
+        <h2> Profile card </h2>
+        <div class="card" style="width: 18rem;"></div>
+      </div>
+
+      </>
+  );
+};
+
+export default Profilecard;
