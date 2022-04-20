e14a11daa8974f32c7d082958164c8863e28ade0
riyasood13 <riya.sood@tothenew.com>
2022-04-20 11:33:45 +0530

    minor changes in Navbar.js

--- frontend/buzz-app/src/Components/Navbar.js
+++ frontend/buzz-app/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useDebugValue } from "react";
+import React from "react";
 import "../Components/Navbar.css";
 import { Link } from "react-router-dom";
 import { Navbar, Container} from "react-bootstrap";
--- frontend/buzz-app/src/Pages/Profile.js
+++ frontend/buzz-app/src/Pages/Profile.js
@@ -14,21 +14,18 @@ const Profile = () => {
                   <CardImg  variant="top" style = {{height: "16rem", width: "100%"}} src="./images/profilebg.jpg" />
                   <div  className="rounded float-left m-4" style={{transform:"translateY(-80px)"}}>
                       <img className="little-profile " src = "./images/nouser.jpg"/>
-                      <div className="">
-                <h2>username</h2>
-                <h4> desciption</h4>
-              </div>
-              <div className> 
-                <Button variant="primary">ADD FRIEND</Button>
-                <Button style={{marginLeft:"1rem"}} variant="outline-danger">View Website</Button>
-              </div>
+                  <div>
+                    <h2>username</h2>
+                    <h4> desciption</h4>
+                  </div>
+                  <div> 
+                    <Button variant="primary">ADD FRIEND</Button>
+                    <Button style={{marginLeft:"1rem"}} variant="outline-danger">View Website</Button>
+                  </div>
                   </div>
           
               </Card.Body>
-              <Card.Text>
               
-              
-              </Card.Text>
             </Card>
           </CardGroup>
      
