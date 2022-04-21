132ff5143a7b4cabb9d5eabe5217666067d17dd5
Prakashttn <prakash.chand@tothenew.com>
2022-04-21 11:20:02 +0530

    Started creating dummy posts for users

--- frontend/buzz-app/src/Components/Profilecard.jsx
+++ frontend/buzz-app/src/Components/Profilecard.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-
-
 const Profilecard = () => {
   return (
       <>
-      
-      <div>
-        <h2> Profile card </h2>
-        <div class="card" style="width: 18rem;"></div>
-      </div>
-
+       <h2>Profile card</h2>
+        
       </>
   );
 };
--- frontend/buzz-app/src/Pages/Feed.js
+++ frontend/buzz-app/src/Pages/Feed.js
@@ -3,12 +3,15 @@ import Leftsidebar from "../Components/Leftsidebar";
 import Profilecard from "../Components/Profilecard";
 import NavigationBar from "../Components/Navbar";
 
+
 const Feed = () => {
   return (
       <>
-      <Profilecard/>
+   
+     
       <h2> This is feed section</h2>
       <Leftsidebar/>
+      <Profilecard/>
       </>
   );
 };
