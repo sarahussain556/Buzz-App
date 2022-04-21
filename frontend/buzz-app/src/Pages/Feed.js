db628223e64c82f638c5d82bb12bc36da7d5ecb5
Prakashttn <prakash.chand@tothenew.com>
2022-04-21 12:01:29 +0530

    Aligning components in feed and styling them

--- frontend/buzz-app/src/Pages/Feed.js
+++ frontend/buzz-app/src/Pages/Feed.js
@@ -1,17 +1,27 @@
 import React from "react";
-import Leftsidebar from "../Components/Leftsidebar";
-import Profilecard from "../Components/Profilecard";
-import NavigationBar from "../Components/Navbar";
+import "./Feed.css"
+// import Leftsidebar from "../Components/Leftsidebar";
+// import Profilecard from "../Components/Profilecard";
+// import NavigationBar from "../Components/Navbar";
+// import { Dummyposts } from "../Components/Dummyposts";
 
 
 const Feed = () => {
   return (
       <>
    
-     
+   {/* <Profilecard/>
       <h2> This is feed section</h2>
       <Leftsidebar/>
-      <Profilecard/>
+     <div>
+       <img src="/images/nightsky.jpg" alt="image" />
+     </div> */}
+    <div className="feedlayout">
+     <h2>Leftside</h2>
+     <h2>Middle</h2>
+     <h2>Rightside</h2>
+     </div>
+
       </>
   );
 };
