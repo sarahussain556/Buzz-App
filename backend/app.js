b9af1fe3724ab68e85d0ee75b130b29adfd04b67
Sara Hussain <85155050+sarahussain556@users.noreply.github.com>
2022-04-22 13:02:26 +0530

    Adding model file of user
    having properies of user eg- username,email,password etc

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,32 @@
+
 const express =require("express");  //node js framework, our server will run on this express
 const app=express();
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); 
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const routeUser=require("./routes/users");
 
 dotenv.config(); //to use dotenv
 
 mongoose.connect(
   process.env.DATABASE,
-
   () => {
     console.log("Connected to MongoDB");
   }
 );
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/",(req,res)=>{
+  res.send("Welcome to homepage");
+})
+
+app.use("/api/users",routeUser);
+//routeUser.initialize(app);
 
 app.listen(8800,()=>{
   console.log("backend server is running");
---
+++ backend/readme.txt
@@ -0,0 +1,6 @@
+  "dotenv": "^16.0.0",   //hides our secret data
+    "express": "^4.17.3",  //node js framework, our server will run on this express
+    "helmet": "^5.0.2",    //when we make request to server it helps us to be secure
+    "mongoose": "^6.3.0",  //helps us to create our mongo model
+    "morgan": "^1.10.0",    //logging middleware which tells us which request we made or what type of request
+    "nodemon": "^2.0.15"    //refreshes the application automatically
