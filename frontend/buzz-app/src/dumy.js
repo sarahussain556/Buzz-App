0de074537c71e655db4e602ab490e58a332bffc4
Prakashttn <prakash.chand@tothenew.com>
2022-04-26 12:22:59 +0530

    Created dummy data

---
+++ frontend/buzz-app/src/dumy.js
@@ -0,0 +1,46 @@
+  export const Posts = [
+    {
+      id: 1,
+      desc: "Love For All, Hatred For None.",
+      photo: "postimages/diningtable.jpg",
+      date: "5 mins ago",
+      userId: 1,
+      like: 10,
+      comment: 9,
+    },
+    
+    {
+      id: 2,
+      desc: "Every moment is a fresh beginning.",
+      photo: "postimages/beach.jpg",
+      date: "1 hour ago",
+      userId: 3,
+      like: 61,
+      comment: 2,
+    },
+    
+    {
+      id: 3,
+      photo: "postimages/morning.jpg",
+      date: "5 hours ago",
+      userId: 5,
+      like: 23,
+      comment: 5,
+    },
+    {
+      id:4,
+      photo: "postimages/moviehall.jpg",
+      date: "1 day ago",
+      userId: 6,
+      like: 20,
+      comment: 6,
+    },
+    {
+      id:5,
+      photo: "postimages/nightsky.jpg",
+      date: "1 day ago",
+      userId: 6,
+      like: 25,
+      comment: 6,
+    },
+  ];
